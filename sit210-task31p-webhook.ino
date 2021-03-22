f0a360b465a5ba9f412e1282ed8f45f7bb65fed1
j-h-fern <68945141+j-h-fern@users.noreply.github.com>
2021-03-22 18:59:00 +1100

    Add files via upload

---
+++ sit210-task31p-webhook.ino
@@ -0,0 +1,59 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_DHT.h>
+
+int led = D7;  // The on-board LED
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11		// DHT 11 
+#define DHTTYPE DHT22		// DHT 22 (AM2302)
+//#define DHTTYPE DHT21		// DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  pinMode(led, OUTPUT);
+  
+  
+  	Serial.begin(9600); 
+	Serial.println("DHTxx test!");
+
+	dht.begin();
+}
+
+void loop() {
+  digitalWrite(led, HIGH);   // Turn ON the LED
+  
+  	float h = dht.getHumidity();
+// Read temperature as Celsius
+	float t = dht.getTempCelcius();
+// Read temperature as Farenheit
+	float f = dht.getTempFarenheit();
+	
+	// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+	
+	String temp = String(t);
+
+  
+  Particle.publish("temp", temp, PRIVATE);
+  delay(30000);               // Wait for 30 seconds
+
+  digitalWrite(led, LOW);    // Turn OFF the LED
+  delay(30000);               // Wait for 30 seconds
+}
+
+
+
+
+
+
